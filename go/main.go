88e2fb9d5fc590149c193b7fa3a239cea15c6d54
Espio24 <63601588+Espio24@users.noreply.github.com>
2020-06-02 23:40:14 +0500

    Финальная версия

--- go/main.go
+++ go/main.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var mass map[string]*Model
+
 type Page struct {
 	Title string
 	Body  []byte
@@ -18,6 +20,19 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+type Model struct {
+	Data      string
+	Aidi      string
+	Name      string
+	Zagolovok string
+	Image     string
+	Text      string
+}
+
+func NewModel(Data, Aidi, Name, Zagolovok, Image, Text string) *Model {
+	return &Model{Data, Aidi, Name, Zagolovok, Image, Text}
 }
 
 func loadPage(title string) (*Page, error) {
@@ -73,6 +88,25 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func viewPostHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("pages/post.html")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	massiv := []*Model{}
+
+	id := r.FormValue("id")
+	post, found := mass[id]
+	massiv = append(massiv, post)
+
+	if !found {
+		http.NotFound(w, r)
+	}
+
+	t.Execute(w, massiv)
+}
+
 func generalHandler(w http.ResponseWriter, r *http.Request) {
 
 	category_name, err := ioutil.ReadFile("Text/category_name.txt")
@@ -81,52 +115,87 @@ func generalHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	category := strings.Split(string(category_name), "\n")
-
-	t, err := template.ParseFiles("pages/general.html")
-	if err != nil {
-		fmt.Print(err)
+	var lengt int = len(category)
+	var a = make([][]string, lengt)
+	var categoryReqest bool = false
+	for i := 0; i < len(category); i++ {
+		a[i] = strings.Split(string(category[i]), "/")
+		namePage := strings.Split(string(category[i]), "/")
+		if "/"+namePage[1]+"/" == r.RequestURI {
+			categoryReqest = true
+		}
 	}
-	t.Execute(w, category)
+	if categoryReqest {
+		categoryHandler(w, r)
+	} else {
+		t, err := template.ParseFiles("pages/general.html")
+		if err != nil {
+			fmt.Print(err)
+		}
+		t.Execute(w, a)
+	}
 
 }
 
 func categoryHandler(w http.ResponseWriter, r *http.Request) {
 
-	category_name, err := ioutil.ReadFile("Text/programmirovanie.txt")
+	category_name, err := ioutil.ReadFile("Text/" + strings.Replace(r.RequestURI, "/", "", -1) + ".txt")
 	if err != nil {
 		fmt.Print(err)
 	}
 
 	header_name := strings.Split(string(category_name), "\n")
 
-	/*for i := 0; i < len(header_name); i++ {
+	var Data string
+	var Aidi string
+	var Name string
+	var Zagolovok string
+	var Image string
+	var Text string
 
-		if header_name[i] != "" {
+	var n int = 0
 
-			configLine := strings.Split(string(header_name[i]), "\n")
+	massiv := []*Model{}
 
-			newHeader := Header{HeaderName: configLine[1]}
-			configs = append(configs, newHeader)
+	for i := 0; i < len(header_name); i++ {
+		switch n {
+		case 0:
+			TexnicText := strings.Split(string(header_name[i]), " ")
+			Data = TexnicText[0]
+			Aidi = TexnicText[1]
+			Name = TexnicText[2]
+		case 1:
+			Zagolovok = header_name[i]
+		case 2:
+			Image = header_name[i]
+		case 3:
+			Text = header_name[i]
+			post := NewModel(Data, Aidi, Name,  Zagolovok, Image, Text)
+			mass[post.Aidi] = post
+			massiv = append(massiv, post)
+		case 4:
+			n = -1
 		}
-	}*/
+		n++
+	}
 
 	t, err := template.ParseFiles("pages/category.html")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	t.Execute(w, header_name)
+	t.Execute(w, massiv)
 
 }
 
 func main() {
+	mass = make(map[string] *Model, 0)
 
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/viewPost/", viewPostHandler)
 	http.HandleFunc("/", generalHandler)
-	http.HandleFunc("/category/", categoryHandler)
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
